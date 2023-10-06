6cf6f3eca3bced6fae3b39871c4d675180fc6f02
heppaat <pali.herrer@gmail.com>
2023-10-06 19:29:03 +0200

    example

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-window.addEventListener('load', function() {
-const rootElement = 
+const arr = [1, 2, 3, 4, 5];
 
-});
+/* function returnDouble(number) {
+  return number * 2;
+}
+
+for (let i = 0; i < arr.length; i++) {
+  console.log(returnDouble(arr[i]));
+} */
+arr.forEach(logDouble);
+
+function logDouble(number) {
+  console.log(number * 2);
+}
+
+/* arr.forEach(function (element) {
+  console.log(element * 2);
+}); */
